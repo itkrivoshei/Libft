adb702026f2ee0436334bb8df990050e5bd72464
Joyce Staunton <jstaunto@ox-n6.21-school.ru>
2019-09-22 16:53:19 +0300

    lol

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: jstaunto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 14:28:17 by jstaunto          #+#    #+#             */
-/*   Updated: 2019/09/22 15:22:09 by jstaunto         ###   ########.fr       */
+/*   Updated: 2019/09/22 16:53:09 by jstaunto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t sz)
 {
 	void	*mem;
 
-	if (!sz)
+	if (sz == 0)
 		return (NULL);
 	if (!(mem = malloc(sz)))
 		return (NULL);
